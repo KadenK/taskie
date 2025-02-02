e92ff1541c9432d1fc053dd5d28bd154558e5593
KadenK <kaden.keep@gmail.com>
2025-02-01 18:13:19 -0700

    remove extra weather icons

--- deployFiles.sh
+++ deployFiles.sh
@@ -1,19 +1,8 @@
 #!/bin/bash
 
-while getopts k:h:s: flag
-do
-    case "${flag}" in
-        k) key=${OPTARG};;
-        h) hostname=${OPTARG};;
-        s) service=${OPTARG};;
-    esac
-done
-
-if [[ -z "$key" || -z "$hostname" || -z "$service" ]]; then
-    printf "\nMissing required parameter.\n"
-    printf "  syntax: deployFiles.sh -k <pem key file> -h <hostname> -s <service>\n\n"
-    exit 1
-fi
+key="~/.ssh/kadenhk-aws.pem"
+hostname="taskie.click"
+service="startup"
 
 printf "\n----> Deploying files for $service to $hostname with $key\n"
 
