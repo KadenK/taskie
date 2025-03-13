caa8914625d0f3844ac315f17e6917ef86b0e609
Kaden Keep <kaden.keep@gmail.com>
2025-03-12 23:47:24 -0600

    add weather skeleton

--- src/task-list/weather.jsx
+++ src/task-list/weather.jsx
@@ -4,21 +4,25 @@ import api from "../app/api";
 
 export function Weather() {
   const [weatherConditions, setWeatherConditions] = React.useState(null);
+  const [isLoading, setIsLoading] = React.useState(true);
 
   useEffect(() => {
     const fetchWeather = async () => {
-      setWeatherConditions(await api.getWeatherConditions());
+      setIsLoading(true);
+      const conditions = await api.getWeatherConditions();
+      setWeatherConditions(conditions);
+      setIsLoading(false);
     };
+    console.log("Fetching weather data...");
     fetchWeather();
   }, []);
 
   const handleWeatherClick = async () => {
-    setWeatherConditions(await api.getWeatherConditions());
+    setIsLoading(true);
+    const conditions = await api.getWeatherConditions();
+    setWeatherConditions(conditions);
+    setIsLoading(false);
   };
-
-  if (!weatherConditions) {
-    return null;
-  }
 
   return (
     <div className="weather-container">
@@ -26,27 +30,45 @@ export function Weather() {
       <div className="weather-items">
         <div className="weather-item weather-today">
           <h4>Today</h4>
-          <img
-            src={weatherConditions.today.condition.icon}
-            alt={weatherConditions.today.condition.name}
-            width="64px"
-            height="64px"
-            onClick={handleWeatherClick}
-            style={{ cursor: "pointer" }}
-          />
-          <span>{weatherConditions.today.temperature} °F</span>
+          {isLoading ? (
+            <>
+              <div className="weather-skeleton-icon" />
+              <span className="weather-skeleton-temp"> --°F </span>
+            </>
+          ) : (
+            <>
+              <img
+                src={weatherConditions.today.condition.icon}
+                alt={weatherConditions.today.condition.name}
+                width="64px"
+                height="64px"
+                onClick={handleWeatherClick}
+                style={{ cursor: "pointer" }}
+              />
+              <span>{weatherConditions.today.temperature} °F</span>
+            </>
+          )}
         </div>
         <div className="weather-item weather-tomorrow">
           <h4>Tomorrow</h4>
-          <img
-            src={weatherConditions.tomorrow.condition.icon}
-            alt={weatherConditions.tomorrow.condition.name}
-            width="64px"
-            height="64px"
-            onClick={handleWeatherClick}
-            style={{ cursor: "pointer" }}
-          />
-          <span>{weatherConditions.tomorrow.temperature} °F</span>
+          {isLoading ? (
+            <>
+              <div className="weather-skeleton-icon" />
+              <span className="weather-skeleton-temp">-- / --°F</span>
+            </>
+          ) : (
+            <>
+              <img
+                src={weatherConditions.tomorrow.condition.icon}
+                alt={weatherConditions.tomorrow.condition.name}
+                width="64px"
+                height="64px"
+                onClick={handleWeatherClick}
+                style={{ cursor: "pointer" }}
+              />
+              <span>{weatherConditions.tomorrow.temperature} °F</span>
+            </>
+          )}
         </div>
       </div>
     </div>
